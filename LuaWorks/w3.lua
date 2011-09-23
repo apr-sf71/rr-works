fdd752a07b1e39dcc0757b938e4faeca6a3a6e09
apr-sf71 <apr-sf71@reflexiverabbit.com>
2011-09-23 21:28:14 +0800

    IM DONE WITH MT YEAH YEAR

--- LuaWorks/w3.lua
+++ LuaWorks/w3.lua
@@ -6,29 +6,36 @@
 module(..., package.seeall)
 
 -- your function leapYear here
-local function leapYear(theYear)
+-- local function leapYear(theYear)
 	-- your calcuation here
-
+	-- every 4 year is a leap ->(theYear % 4) == 0
+	--but every 100 year is not -->(theYear % 100) != 0
+	--but every 400 year is a leap --> (theYear % 400) == 0
 	-- e.g.
-	if (theYear == 4) then
-		return true
-	else 
+
+local function yeahyear (theYear)
+	if (theYear % 4)== 0  then
+		if ((theYear % 100) == 0) then
+			if ((theYear % 400) == 0) then 
+				return true
+			else
+				return false
+			end
+		else
+			return true
+		end
+	else
 		return false
 	end
-
 end
 
 run = function()
    -- call your function on year xxxx
-	local year1 = 1984
-	local result1 = leapYear(year1)
-	if (result == true) then
+	local year1 = 2004
+	local yeahyear1 = yeahyear(year1)
+	if (yeahyear1 == true) then
 		WorkPad1:setText("year " .. year1 .. " is leap ")
 	else
 		WorkPad1:setText("year " .. year1 .. " is not leap ")
 	end
-
-   -- call your function on year yyyy
-   -- and put on WorkPad2
-
 end
